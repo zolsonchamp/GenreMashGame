0f2d9df4a96c0447a881f9a5c2a0039fb2a30bb7
jacob okoshi <jokoshi42@gmail.com>
2024-01-30 16:02:36 -0500

    added documentation to code and adjusted camera rotation

--- OSProject/Assets/Scripts/moveCamera.cs
+++ OSProject/Assets/Scripts/moveCamera.cs
@@ -21,49 +21,61 @@ public class moveCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //moves camera up
         if (Input.GetKey(KeyCode.W))
         {
-            Camera.main.transform.position= new Vector3(Camera.main.transform.position.x,Camera.main.transform.position.y,Camera.main.transform.position.z+cameraSpeedVert);
-            if (Camera.main.transform.position.z > cameraBoundsVert)
+            Camera.main.transform.position= new Vector3(Camera.main.transform.position.x - cameraSpeedVert, Camera.main.transform.position.y,Camera.main.transform.position.z);
+            //restricts camera to map
+            if (Camera.main.transform.position.x < cameraBoundsVert*-1)
             {
-                Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, cameraBoundsVert);
+                Camera.main.transform.position = new Vector3(cameraBoundsVert*-1, Camera.main.transform.position.y, Camera.main.transform.position.z);
             }
         }
+        //moves camera down
         if (Input.GetKey(KeyCode.S))
         {
-            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z - cameraSpeedVert);
-            if (Camera.main.transform.position.z < cameraBoundsVert*-1)
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x + cameraSpeedVert, Camera.main.transform.position.y, Camera.main.transform.position.z);
+            //restricts camera to map
+            if (Camera.main.transform.position.x > cameraBoundsVert)
             {
-                Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, cameraBoundsVert*-1);
+                Camera.main.transform.position = new Vector3(cameraBoundsVert, Camera.main.transform.position.y, Camera.main.transform.position.z);
             }
         }
+        //moves camera left
         if (Input.GetKey(KeyCode.A))
         {
-            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x-cameraSpeedHoriz, Camera.main.transform.position.y, Camera.main.transform.position.z);
-            if (Camera.main.transform.position.x < cameraBoundsHoriz*-1)
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z-cameraSpeedHoriz);
+            //restricts camera to map
+            if (Camera.main.transform.position.z < cameraBoundsHoriz*-1)
             {
-                Camera.main.transform.position = new Vector3(cameraBoundsHoriz*-1, Camera.main.transform.position.y, Camera.main.transform.position.z);
+                Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, cameraBoundsHoriz*-1);
             }
         }
+        //moves camera right
         if (Input.GetKey(KeyCode.D))
         {
-            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x+cameraSpeedHoriz, Camera.main.transform.position.y, Camera.main.transform.position.z);
-            if (Camera.main.transform.position.x > cameraBoundsHoriz)
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z+cameraSpeedHoriz);
+            //restricts camera to map
+            if (Camera.main.transform.position.z > cameraBoundsHoriz)
             {
-                Camera.main.transform.position = new Vector3( cameraBoundsHoriz, Camera.main.transform.position.y, Camera.main.transform.position.z);
+                Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, cameraBoundsHoriz);
             }
         }
+        //zooms camera in
         if (Input.mouseScrollDelta.y > 0)
         {
             Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y-cameraZoom, Camera.main.transform.position.z);
+            //sets limit to zoom in
             if (Camera.main.transform.position.y < cameraBoundsZoomIn)
             {
                 Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, cameraBoundsZoomIn, Camera.main.transform.position.z);
             }
         }
+        //zooms camera out
         if (Input.mouseScrollDelta.y < 0)
         {
             Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y + cameraZoom, Camera.main.transform.position.z);
+            //sets limit to zoom out
             if (Camera.main.transform.position.y > cameraBoundsZoomOut)
             {
                 Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, cameraBoundsZoomOut, Camera.main.transform.position.z);
--- OSProject/Assets/Scripts/placeWall.cs
+++ OSProject/Assets/Scripts/placeWall.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class placeWall : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class placeWall : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //sets wall placement rotation at start
         transform.localRotation = Quaternion.Euler(0, 0, 0);
         wall.transform.localRotation = Quaternion.Euler(0, 0, 0);
     }
@@ -20,24 +22,29 @@ public class placeWall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //converts mouse position to a world position through camera
         screenPosition = Input.mousePosition;
         Ray ray = Camera.main.ScreenPointToRay(screenPosition);
         if(Physics.Raycast(ray, out RaycastHit hitData, 500, layersToHit))
         {
             worldPosition= hitData.point;
         }
+        //corrects height of mouse to world position and moves wall object to mouse
         correctedWorldPosition = new Vector3(worldPosition.x, 0, worldPosition.z);
         transform.position = correctedWorldPosition;
+        //on left click place a wall
         if (Input.GetMouseButtonUp(0))
         {
             wall.transform.position = correctedWorldPosition;
             Instantiate(wall);
         }
+        //rotate wall clockwise
         if (Input.GetKeyUp(KeyCode.R))
         {
             transform.Rotate(0.0f, 15.0f, 0.0f, Space.World);
             wall.transform.Rotate(0.0f, 15.0f, 0.0f, Space.World);
         }
+        //rotate wall counterclockwise
         if (Input.GetKeyUp(KeyCode.Q))
         {
             transform.Rotate(0.0f, -15.0f, 0.0f, Space.World);
